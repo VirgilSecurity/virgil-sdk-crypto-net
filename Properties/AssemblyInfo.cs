c064cab211b17ab1caff24bddcc30c807cb4627c
Vasilina Bezuglaya <Vasilina Bezuglaya>
2018-02-21 13:23:03 +0500

    initial commit

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,34 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Virgil.CryptoImpl")]
+[assembly: AssemblyDescription("Virgil.CryptoImpl provides implementations for Virgil.SDK.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Virgil.CryptoImpl")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: InternalsVisibleTo("Virgil.CryptoImpl.Tests")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("b5144a99-d9ba-4bc6-aa1b-49143b2faca9")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ VirgilAccessTokenSigner.cs
@@ -0,0 +1,95 @@
+﻿#region Copyright (C) Virgil Security Inc.
+
+// Copyright (C) 2015-2018 Virgil Security Inc.
+// 
+// Lead Maintainer: Virgil Security Inc. <support@virgilsecurity.com>
+// 
+// All rights reserved.
+// 
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions 
+// are met:
+// 
+//   (1) Redistributions of source code must retain the above copyright
+//   notice, this list of conditions and the following disclaimer.
+//   
+//   (2) Redistributions in binary form must reproduce the above copyright
+//   notice, this list of conditions and the following disclaimer in
+//   the documentation and/or other materials provided with the
+//   distribution.
+//   
+//   (3) Neither the name of the copyright holder nor the names of its
+//   contributors may be used to endorse or promote products derived 
+//   from this software without specific prior written permission.
+// 
+// THIS SOFTWARE IS PROVIDED BY THE AUTHOR ''AS IS'' AND ANY EXPRESS OR
+// IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+// WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+// DISCLAIMED. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT,
+// INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+// (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+// SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+// HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+// STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING
+// IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+// POSSIBILITY OF SUCH DAMAGE.
+
+#endregion
+using Virgil.CryptoAPI;
+
+namespace Virgil.CryptoImpl
+{
+    /// <summary>
+    /// The <see cref="VirgilAccessTokenSigner"/> implements <see cref="IAccessTokenSigner"/> interface and
+    ///provides a cryptographic operations in applications, such as signature generation and verification in an access token.
+    /// </summary>
+    public class VirgilAccessTokenSigner : IAccessTokenSigner
+    {
+        private readonly string algorithm = "VEDS512";
+        private readonly VirgilCrypto virgilCrypto;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="VirgilAccessTokenSigner" /> class.
+        /// </summary>
+        public VirgilAccessTokenSigner()
+        {
+            virgilCrypto = new VirgilCrypto();
+        }
+
+        /// <summary>
+        /// Generates the digital signature for the specified <paramref name="tokenBytes"/> using
+        /// the specified <see cref="IPrivateKey"/>
+        /// </summary>
+        /// <param name="tokenBytes">The material representation bytes of access token 
+        /// for which to compute the signature.</param>
+        /// <param name="privateKey">The private key</param>
+        /// <returns>The digital signature for the material representation bytes of access token.</returns>
+        public byte[] GenerateTokenSignature(byte[] tokenBytes, IPrivateKey privateKey)
+        {
+            return virgilCrypto.GenerateSignature(tokenBytes, privateKey);
+        }
+
+        /// <summary>
+        /// Represents used signature algorithm.
+        /// </summary>
+        /// <returns></returns>
+        public string GetAlgorithm()
+        {
+            return algorithm;
+        }
+
+        /// <summary>
+        /// Verifies that a digital signature is valid by checking the <paramref name="signature"/> and
+        /// provided <see cref="IPublicKey"/> and <paramref name="tokenBytes"/>.
+        /// </summary>
+        /// <param name="tokenBytes">The material representation bytes of access token 
+        /// for which the <paramref name="signature"/> has been generated.</param>
+        /// <param name="signature">The digital signature for the <paramref name="tokenBytes"/></param>
+        /// <param name="publicKey">The public key</param>
+        /// <returns>True if signature is valid, False otherwise.</returns>
+        public bool VerifyTokenSignature(byte[] signature, byte[] tokenBytes, IPublicKey publicKey)
+        {
+            return virgilCrypto.VerifySignature(signature, tokenBytes, publicKey);
+        }
+    }
+}
