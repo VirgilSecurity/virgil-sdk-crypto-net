949898ba85d8f53ec77df9b7fd58d1290368c47a
Vasilina Bezuglaya <Vasilina Bezuglaya>
2018-02-22 12:41:34 +0500

    delete unrequired referencies

---
+++ Virgil.CryptoImpl.Tests/AppSettings.cs
@@ -0,0 +1,11 @@
+﻿using System.Configuration;
+
+namespace Virgil.CryptoImpl.Tests
+{
+    public class AppSettings
+    {
+        public static string PrivateKeySTC31_1 = ConfigurationManager.AppSettings["test:PrivateKeySTC31_1"];
+        public static string PrivateKeySTC31_2 = ConfigurationManager.AppSettings["test:PrivateKeySTC31_2"];
+        public static string PublicKeySTC32 = ConfigurationManager.AppSettings["test:PublicKeySTC32"];
+    }
+}
