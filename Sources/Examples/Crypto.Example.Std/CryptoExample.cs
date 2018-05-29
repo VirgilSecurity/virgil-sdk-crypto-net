690734d6f612778facd485d84b1744422517b59a
Vasilina <marfachaiko@gmail.com>
2018-05-29 17:33:37 +0500

    auto-generate nuget package for Virgil.CryptoAPI

---
+++ Sources/Examples/Crypto.Example.Std/CryptoExample.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Virgil.Crypto;
+
+namespace Crypto.Example.Std
+{
+    public class CryptoExample
+    {
+        public CryptoExample()
+        {
+            var version = VirgilVersion.AsString();
+            var pfs = new Virgil.Crypto.Pfs.VirgilPFS();
+            var cryptoHighLevel = new Virgil.Crypto.VirgilCrypto();
+            var pythia = new Virgil.Crypto.Pythia.VirgilPythia();
+        }
+    }
+}
---
+++ Sources/Virgil.Crypto.Shared/IKeyPair.cs
@@ -0,0 +1,12 @@
+﻿using Virgil.CryptoAPI;
+
+namespace Virgil.Crypto
+{
+    public interface IKeyPair<TPublicKey, TPrivateKey>
+        where TPublicKey : IPublicKey
+        where TPrivateKey : IPrivateKey
+    {
+        TPublicKey PublicKey { get; }
+        TPrivateKey PrivateKey { get; }
+    }
+}
