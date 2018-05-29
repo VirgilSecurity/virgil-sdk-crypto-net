cf8a55c04ebf491e5943ded872e835fa49c40b51
Vasilina <marfachaiko@gmail.com>
2018-05-29 17:31:04 +0500

    added cryptoAPI project from SDK

---
+++ Virgil.CryptoAPI/IKeyPair.cs
@@ -0,0 +1,12 @@
+﻿using Virgil.CryptoAPI;
+
+namespace Virgil.Crypto
+{
+    public class IKeyPair<TPublicKey, TPrivateKey>
+        where TPublicKey : IPublicKey
+        where TPrivateKey : IPrivateKey
+    {
+        public TPublicKey PublicKey { get; }
+        public TPrivateKey PrivateKey { get; }
+    }
+}
